114299d0dd9033289118c64701e3e4be93d644b9
Ilnur Safin <safin3010@gmail.com>
2026-09-17 21:35:50 +0500

    docunent_reader add

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from config.config import load_config
 from server import run_server
+from document_reader import load_documents
 
 if __name__ == '__main__':
     config = load_config('./config/config.yaml')
+    load_documents(config.search_paths)
     print(config.search_paths)
 
     run_server()
